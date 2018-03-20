cae8da0a0891951e9e5b96922a1758334775231e
Ely Alamillo <ely0013@gmail.com>
2018-03-19 22:23:52 -0500

    moved to typescript 2.7

---
+++ build/jsonModule.js
@@ -0,0 +1,89 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+class jsonModule {
+    constructor() {
+        if (jsonModule.instance) {
+            throw new Error('YOu cannot create an object of this type. Try using jsonModule.getInstance()');
+        }
+    }
+    saveJSON(json) {
+        // save to DISK
+        let path = `${this._path}/${this._name}`;
+        const data = JSON.stringify(json);
+        let pathExist = false;
+        try {
+            fs.accessSync(path);
+            pathExist = true;
+        }
+        catch (error) {
+            throw error;
+        }
+        if (!path) {
+            // crete the path
+            fs.mkdirSync(this._path);
+        }
+        // writes out to DISK
+        fs.writeFileSync(path, data, 'utf8');
+    }
+    getJSON(json) {
+        // return JSON
+        const promise = new Promise((resolve, reject) => {
+            if (jsonModule.isset(this._name && jsonModule.isset(this._path))) {
+                let name = `${this._path}/${this._name}`;
+                if (fs.lstatSync(name).isFile) {
+                    // file exists
+                    fs.readFile(name, 'utf8', (err, data) => {
+                        if (err)
+                            reject(err);
+                        resolve(JSON.parse(data));
+                    });
+                }
+                else {
+                    reject(new Error('Invalid file! Path can not be resolved'));
+                }
+            }
+            else {
+                reject(new Error('Path and name are required.'));
+            }
+        });
+        return promise;
+    }
+    get path() {
+        // get path
+        return this._path;
+    }
+    set path(path) {
+        // set path
+        this._path = path;
+    }
+    get name() {
+        // get name
+        return this._name;
+    }
+    set name(name) {
+        // set name
+        this._name = name;
+    }
+    static getInstance(name, path) {
+        // get local instance
+        if (jsonModule.instance === undefined) {
+            jsonModule.instance = new jsonModule();
+        }
+        if (jsonModule.isset(name)) {
+            jsonModule.instance._name = name;
+        }
+        if (jsonModule.isset(path)) {
+            jsonModule.instance._path = path;
+        }
+        return jsonModule.instance;
+    }
+    static isset(value) {
+        let set = false;
+        if (value !== undefined || value !== null) {
+            set = true;
+        }
+        return set;
+    }
+}
+exports.jsonModule = jsonModule;
--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/app-root-path/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/app-root-path/index.d.ts" />
