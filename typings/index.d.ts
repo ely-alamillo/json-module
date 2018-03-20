1787eb1a1d5f28e7a57b5b857ec576b942277f37
Ely Alamillo <ely0013@gmail.com>
2018-03-19 19:30:33 -0500

    added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/app-root-path/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/app-root-path/index.d.ts" />
